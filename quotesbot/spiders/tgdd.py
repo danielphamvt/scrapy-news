d36a16e2dec9deca18c82ae44e962a624be13fef
Tuan PHAM <phamtuantkt@gmail.com>
2020-09-08 14:01:22 +0700

    Add files via upload

---
+++ quotesbot/spiders/tgdd.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SpiderTGDD(scrapy.Spider):
+    name = 'thegioididong'
+    start_urls = [
+        'https://www.thegioididong.com/dtdd/samsung-galaxy-a21s/danh-gia',
+    ]
+
+    def parse(self, response):
+        for comment in response.xpath('//div[@class="rc"]'):
+            rating = comment.xpath('.//p/span').extract_first()
+            if rating is None:
+                rating = []
+            if "iconcom-txtstar" in rating:
+                ratings = rating.split('</i>')
+                star = 0
+                for i, value in enumerate(ratings):
+                    if "iconcom-txtstar" in value:
+                        star += 1
+                rated = star
+                yield {
+                    'text': comment.xpath('.//p/i/text()').extract_first(),
+                    'rating': rated,
+                    'date': comment.xpath('//a[@class="cmtd"]/text()').extract_first(),
+                    'url': response.urljoin('')
+                }
+
+        # for url in start_urls:
+        #     yield scrapy.Request(response.urljoin(url))
+        next_page_url = response.xpath("//div[@class='pagcomment']/span[@class='active']/text()").extract_first()
+        if next_page_url is not None:
+            next_page_url = 'https://www.thegioididong.com/dtdd/samsung-galaxy-a51/danh-gia'\
+                            + '?p='+str(1+int(next_page_url))
+            yield scrapy.Request(next_page_url)
